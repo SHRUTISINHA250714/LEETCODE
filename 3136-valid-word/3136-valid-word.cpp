6064c0a7a34326bcb532be59d6dd26e7c008a065
SHRUTISINHA250714 <157522205+SHRUTISINHA250714@users.noreply.github.com>
2024-05-05 13:04:12 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (75.00%) - LeetHub

--- 3136-valid-word/3136-valid-word.cpp
+++ 3136-valid-word/3136-valid-word.cpp
@@ -1,49 +1,53 @@
 class Solution {
 public:
     bool isValid(string word) {
-//         if (word.size() < 3)
-//         return false;
-//         bool vowelFound = false;
-//         bool alphaFound=false;
-//         for (int i = 0; i < word.size(); i++) {
-//             if (isalpha(word[i])|| isalnum(word[i])) {
-//                 if ((word[i] == 'a' || word[i] == 'A' || word[i] == 'e' || word[i] == 'E' || word[i] == 'i' || word[i] == 'I' || word[i] == 'o' || word[i] == 'O' || word[i] == 'u' || word[i] == 'U')) {
-//                     vowelFound = true;
-//                 }
-//                 else if(isdigit(word[i]))
-//                     continue;
-//                 else{
-//                     alphaFound = true;
-//                 }
-//         }else {
-//             return false;
-//         }
-//     }
-    
-//     return (vowelFound && alphaFound);
-    // }
-    if (word.size() < 3)
+        if (word.size() < 3)
         return false;
-
-    unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-    bool vowelFound = false;
-    bool alphaFound = false;
-
-    for (char c : word) {
-        if (isalnum(c)) {
-            if (vowels.count(c)) {
-                vowelFound = true;
-            } else if (isdigit(c)) {
-                continue;
-            } else {
-                alphaFound = true;
-            }
-        } else {
+        bool vowelFound = false;
+        bool alphaFound=false;
+        for (int i = 0; i < word.size(); i++) {
+            if (isalpha(word[i])|| isalnum(word[i])) {
+                if ((word[i] == 'a' || word[i] == 'A' || word[i] == 'e' || word[i] == 'E' || word[i] == 'i' || word[i] == 'I' || word[i] == 'o' || word[i] == 'O' || word[i] == 'u' || word[i] == 'U')) {
+                    vowelFound = true;
+                }
+                else if(isdigit(word[i]))
+                    continue;
+                else{
+                    alphaFound = true;
+                }
+        }else {
             return false;
         }
     }
     
     return (vowelFound && alphaFound);
-}
+        
+    }
+};      
     
-};
+//     return (vowelFound && alphaFound);
+    // }
+//     if (word.size() < 3)
+//         return false;
+
+//     unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+//     bool vowelFound = false;
+//     bool alphaFound = false;
+
+//     for (char c : word) {
+//         if (isalnum(c)) {
+//             if (vowels.count(c)) {
+//                 vowelFound = true;
+//             } else if (isdigit(c)) {
+//                 continue;
+//             } else {
+//                 alphaFound = true;
+//             }
+//         } else {
+//             return false;
+//         }
+//     }
+    
+//     return (vowelFound && alphaFound);
+// }
+// };
