f53a025b774f497074e67dffdd9e92f72199a900
SHRUTISINHA250714 <157522205+SHRUTISINHA250714@users.noreply.github.com>
2024-05-10 17:25:15 +0530

    Time: 302 ms (50.68%), Space: 120.6 MB (32.50%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-         vector<float>fraction;
-        for(int i=0;i<arr.size();i++){
-            for(int j=i+1;j<arr.size();j++){
-                float frac=static_cast<float>(arr[i]) / static_cast<float>(arr[j]);
-                fraction.push_back(frac);
-            }
-        }
-        sort(fraction.begin(),fraction.end());
-        float temp=fraction[k-1];
-        vector<int> array;
-        for(int i=0;i<arr.size();i++){
-            for(int j=i+1;j<arr.size();j++){
-                float frac=static_cast<float>(arr[i])/static_cast<float>(arr[j]);
-                if(frac==temp){
-                    array.push_back(arr[i]);
-                    array.push_back(arr[j]);
-                    break;
-                }
-            }
-        }
-        return array;
+//          vector<float>fraction;
+//         for(int i=0;i<arr.size();i++){
+//             for(int j=i+1;j<arr.size();j++){
+//                 float frac=static_cast<float>(arr[i]) / static_cast<float>(arr[j]);
+//                 fraction.push_back(frac);
+//             }
+//         }
+//         sort(fraction.begin(),fraction.end());
+//         float temp=fraction[k-1];
+//         vector<int> array;
+//         for(int i=0;i<arr.size();i++){
+//             for(int j=i+1;j<arr.size();j++){
+//                 float frac=static_cast<float>(arr[i])/static_cast<float>(arr[j]);
+//                 if(frac==temp){
+//                     array.push_back(arr[i]);
+//                     array.push_back(arr[j]);
+//                     break;
+//                 }
+//             }
+//         }
+//         return array;
         
         //concept of returning pair
-        // vector<pair<double, pair<int, int>>> fractions;
-        // int n = arr.size();
-        // for(int i = 0; i < n - 1; i++) {
-        //     for(int j = i + 1; j < n; j++) {
-        //         double frac = (double)arr[i] / arr[j];
-        //         fractions.push_back({frac, {arr[i], arr[j]}});
-        //     }
-        // }
-        // sort(fractions.begin(), fractions.end());
-        // return {fractions[k - 1].second.first, fractions[k - 1].second.second};
+        vector<pair<double, pair<int, int>>> fractions;
+        int n = arr.size();
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                double frac = (double)arr[i] / arr[j];
+                fractions.push_back({frac, {arr[i], arr[j]}});
+            }
+        }
+        sort(fractions.begin(), fractions.end());
+        vector <int> result;
+        result.push_back(fractions[k - 1].second.first);
+        result.push_back(fractions[k - 1].second.second);
+        return result;
 
  
         
