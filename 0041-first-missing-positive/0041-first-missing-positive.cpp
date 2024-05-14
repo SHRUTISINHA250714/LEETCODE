94ce0c589b8fec55a8bbf45baa91fddab54b0262
SHRUTISINHA250714 <157522205+SHRUTISINHA250714@users.noreply.github.com>
2024-05-14 19:40:11 +0530

    Time: 71 ms (19.30%), Space: 54.6 MB (18.04%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-//         sort(nums.begin(),nums.end());
-//         if(nums.size()==1){
-//             if (nums[0]==1)
-//                 return 2;
-//             else
-//                 return 1;
+        sort(nums.begin(), nums.end());
+    
+    int n = nums.size();
+    
+    if (n == 0 || nums[n - 1] <= 0) 
+        return 1;
+    
+    nums.erase(remove_if(nums.begin(), nums.end(), [](int x) { return x <= 0; }), nums.end());
+
+    n = nums.size();
+
+    if (n == 0 || nums[0] != 1) 
+        return 1;
+    
+    for (int i = 0; i < n - 1; i++) {
+        if (nums[i + 1] - nums[i] > 1) 
+            return nums[i] + 1;
+    }
+    
+    return nums[n - 1] + 1;
+    
+//         unordered_set<int> seen;
+//         for (int num : nums) {
+//            if (num > 0)
+//               seen.insert(num);
+//            }
+//         for (int i = 1; i <= nums.size() + 1; ++i) {
+//              if (seen.find(i) == seen.end())
+//                 return i;
 //         }
-            
         
-        
-//         for(int i=1;i<nums.size();i++){
-//             if(nums[i]<0)
-//                 i++;
-//             else{
-//                 auto it = find(nums.begin(), nums.end(),i );
-//                 if (it == nums.end()) {
-//                     return i;
-//                 }
-                    
-//             }
-            
-//         }
-//         return nums.size();
-//     }
-        unordered_set<int> seen;
-        for (int num : nums) {
-           if (num > 0)
-              seen.insert(num);
-           }
-        for (int i = 1; i <= nums.size() + 1; ++i) {
-             if (seen.find(i) == seen.end())
-                return i;
-        }
-        
-        return 1; 
+//         return 1; 
     }
         
     
