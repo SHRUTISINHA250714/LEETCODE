3b4892134fe81ca89a32c1aa7c6046b45c22064c
SHRUTISINHA250714 <157522205+SHRUTISINHA250714@users.noreply.github.com>
2024-07-24 21:04:14 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (55.66%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,27 +1,44 @@
 class Solution {
-private:
-    void solve(vector<int> nums, vector<int> output, int index, vector<vector<int> >& ans) {
-        //base case
-        if(index >= nums.size()) {
-            ans.push_back(output);
-            return ;
-        }
+// private:
+//     void solve(vector<int> nums, vector<int> output, int index, vector<vector<int> >& ans) {
+//         //base case
+//         if(index >= nums.size()) {
+//             ans.push_back(output);
+//             return ;
+//         }
         
-        //exclude
-        solve(nums, output, index+1, ans);
+//         //exclude
+//         solve(nums, output, index+1, ans);
         
-        //include
-        int element = nums[index];
-        output.push_back(element);
-        solve(nums, output, index+1, ans);
+//         //include
+//         int element = nums[index];
+//         output.push_back(element);
+//         solve(nums, output, index+1, ans);
         
-    }
+//     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
+    //     vector<vector<int> > ans;
+    //     vector<int> output;
+    //     int index = 0;
+    //     solve(nums, output, index, ans);
+    //     return ans;
+    // }
         vector<vector<int> > ans;
         vector<int> output;
-        int index = 0;
-        solve(nums, output, index, ans);
+        int subsets=1<<nums.size();
+        for (int num=0;num<subsets;num++){
+            for(int i=0;i<nums.size();i++){
+                if(num&(1<<i)){
+                    output.push_back(nums[i]);
+                }
+            } 
+            ans.push_back(output);
+            output={};
+        }
         return ans;
     }
+        
+        
+    
 };
