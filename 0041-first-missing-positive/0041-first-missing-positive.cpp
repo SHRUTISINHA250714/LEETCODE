e04021dcb4190ed9b3e77964690e63e80c9eca3f
SHRUTISINHA250714 <157522205+SHRUTISINHA250714@users.noreply.github.com>
2024-11-12 18:44:54 +0530

    Time: 72 ms (14.34%), Space: 78.3 MB (14.57%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
+        // int n = nums.size();
+        // if (n == 0 || nums[n - 1] <= 0) 
+        // return 1;
+        // nums.erase(remove_if(nums.begin(), nums.end(), [](int x) { return x <= 0; }), nums.end());
+        // n = nums.size();
+        // if (n == 0 || nums[0] != 1) 
+        //     return 1;
+        // for (int i = 0; i < n - 1; i++) {
+        //     if (nums[i + 1] - nums[i] > 1) 
+        //         return nums[i] + 1;
+        // }
+        // return nums[n - 1] + 1;
     
-    int n = nums.size();
-    
-    if (n == 0 || nums[n - 1] <= 0) 
-        return 1;
-    
-    nums.erase(remove_if(nums.begin(), nums.end(), [](int x) { return x <= 0; }), nums.end());
-
-    n = nums.size();
-
-    if (n == 0 || nums[0] != 1) 
-        return 1;
-    
-    for (int i = 0; i < n - 1; i++) {
-        if (nums[i + 1] - nums[i] > 1) 
-            return nums[i] + 1;
-    }
-    
-    return nums[n - 1] + 1;
-    
-//         unordered_set<int> seen;
-//         for (int num : nums) {
-//            if (num > 0)
-//               seen.insert(num);
-//            }
-//         for (int i = 1; i <= nums.size() + 1; ++i) {
-//              if (seen.find(i) == seen.end())
-//                 return i;
-//         }
+        unordered_set<int> seen;
+        for (int num : nums) {
+           if (num > 0)
+              seen.insert(num);
+           }
+        for (int i = 1; i <= nums.size() + 1; ++i) {
+             if (seen.find(i) == seen.end())
+                return i;
+        }
         
-//         return 1; 
+        return 1; 
     }
         
     
